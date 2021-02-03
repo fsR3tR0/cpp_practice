09b46f4a4bb2f2b6bacc837636ddd98697107e7f
R3tR0@dell <fsR3tR0@github.com>
2021-02-03 13:04:01 -0500

    -

--- test0/main.cpp
+++ test0/main.cpp
@@ -6,25 +6,22 @@ int main(int argc, char *argv[])
 {
 	setlocale(LC_ALL, "hun");
 	u16 in_u8 = 0;
-	string in_str;
+	string in_str, in_str2;
 
 	cout<<"Halo, ez meg nem ekezetes"<<endl;
 	cout<<"Ez már az: űűűöüúőpí"<<endl;
 
 	cout<<"Irjon be egy szamot:";
 	cin>>in_u8;
+	cin.ignore();
 	cout<<"Beolvasott szam erteke: "<<in_u8<<endl;
 	cout<<"Irjon be egy szoveget."<<endl;
-	cin>>in_str;
-//	while(getline(cin,in_str) != '0'){};
-//	cin.getline(cin,in_str);
-	cout<<"Ezt irtad be: "<<in_str<<endl;
-	cout<<"Meret: "<<in_str.size()<<", hossz: "<<in_str.length()<<endl;
-	cout<<"Meg egy:";
-	getline(cin,in_str);
-	cout<<in_str<<endl;
+	getline(cin,in_str2);
+	cout<<in_str2<<endl;
+	cout<<"A szoveg merete: "<<in_str2.size()<<" - hossza: "<<in_str2.length()<<endl;
 //	cout<<"Forditva: ";
-
+	cin>>in_u8;
+	cout<<in_u8<<endl;
 //	system("echo 3s...");
 //	system("sleep 3");
 //	system("clear");
--- test0/test.cpp
+++ test0/test.cpp
@@ -4,7 +4,11 @@ using namespace std;
 
 int main()
 {
-	string data;
+	string data, data_2;
+
+	cout<<"Irj be valamit"<<endl;
+	cin>>data_2;
+	cout<<data_2<<endl<<"Meg valamit"<<endl;
 	getline(cin,data);
 	cout<<data<<endl;
 
